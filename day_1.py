f0c41e1c4ffbe1e563e281bf2df8c5326e8cd980
aoik109 <allison.ko.family@gmail.com>
2023-11-04 20:30:37 -0700

    created a username creator

---
+++ day_1.py
@@ -0,0 +1,25 @@
+# November 4, 2023
+# Learned print(), input(), len(), variables
+
+## Purpose of this program: Username creator
+print("Hi! What is your name?")
+name = input()
+
+print("Nice to meet you, " + name)
+print("I will ask you questions to develop your username:")
+
+print("What is your favorite color?")
+color = input()
+
+print("What are your favorite two numbers?")
+print("First number:")
+num1 = input()
+print("Second number:")
+num2 = input()
+print("Very cool!")
+num = num1 * num2
+
+print("Lastly, what is your favorite type of animal?")
+animal = input()
+
+print("Awesome! Your username is: " + name + num + animal)
